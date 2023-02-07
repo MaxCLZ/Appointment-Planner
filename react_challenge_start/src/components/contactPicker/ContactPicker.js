26dc3fc9c265b66295fcd5119fb4d56a0226a613
MaxCLZ <maxence.clusaz@gmail.com>
2023-02-07 13:16:35 +0100

    Update ContactPicker.js
    fix required contact

--- react_challenge_start/src/components/contactPicker/ContactPicker.js
+++ react_challenge_start/src/components/contactPicker/ContactPicker.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const ContactPicker = ({ contacts, onChange }) => {
   return (
-    <select key="-1" name="contacts" onChange={onChange}>
-      <option key="default" value="-1">
+    <select required name="contacts" onChange={onChange}>
+      <option key="default" value="">
         Chose a contact
       </option>
       {contacts?.map((contact, index) => (
